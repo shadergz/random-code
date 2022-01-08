e07f731e89e0d9f370a6edff2f1aced9297a2e47
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-01-08 14:57:20 -0300

    Comment fixed

--- sqrt_newton.c
+++ sqrt_newton.c
@@ -8,9 +8,9 @@
 
 int main (int argc, char **argv)
 {
-	/* 3 < sqrt (10) < 3.20
-	 * sqrt (10) = x*x
-	 * x^2 = 10
+	/*	3 < sqrt (10) < 3.20
+	 *	sqrt (10) = x*x
+	 *	x^2 = 10
 	*/
 	double value = 2;
 	for (int i = 0; i < 10; i++)
