63dbaa52a8bd6ff155410cd551cdfde620f508a1
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-01-08 14:50:14 -0300

    Sqrt from Newton method added

---
+++ README.md
@@ -0,0 +1,13 @@
+# random-code
+
+A ramdom code collection used for SFW purposes
+
+## Codes that used aproximation methods
+
+### Square root with Newton method
+
+```c
+double value = 2;
+for (int i = 0; i < 10; i++)
+    value -= (value * value - x) / (value * 2);
+```
---
+++ sqrt_newton.c
@@ -0,0 +1,21 @@
+/*
+ *	Source code written by Gabriel Correia
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main (int argc, char **argv)
+{
+	/* 3 < sqrt (10) < 3.20
+	 * sqrt (10) = x*x
+	 * x^2 = 10
+	*/
+	double value = 2;
+	for (int i = 0; i < 10; i++)
+		value -= (value * value - x) / (value * 2);
+	printf ("SQRT (%d) = ~= %lf\n", x, value);
+	return 0;
+	
+}
